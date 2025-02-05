dbe8274cb5362a387653d424b34920a5f7c65d83
Milana <milana15.10.05@gmail.com>
2025-02-05 14:14:25 -0500

    Update hamburger-menu
    In this commit the index.html and main.css files were updated with some basic functionality, like hover menu, hamburger menu, and some responsiveness added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 (() => {
+
+    //hamburger menu 
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.menu');
+
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    })
     
 //FAQ 
   const faqs = document.querySelectorAll('.faq');
-
   faqs.forEach(faq => {
     faq.addEventListener("click", () => {
       faq.classList.toggle('openFaq');
