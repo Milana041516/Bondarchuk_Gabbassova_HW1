d714c5259febd0e3583ded7f37262757696c351b
Milana <milana15.10.05@gmail.com>
2025-02-05 15:14:50 -0500

    Add gsap animations
    In this commit basic gsap animations were added

--- js/main.js
+++ js/main.js
@@ -9,11 +9,52 @@
         navMenu.classList.toggle("active");
     })
     
-//FAQ 
-  const faqs = document.querySelectorAll('.faq');
-  faqs.forEach(faq => {
+    //scrolling links
+    gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+
+    const navLinks = document.querySelectorAll('#nav-menu ul li a');
+
+    function scrollLink(e) {
+        e.preventDefault();
+        console.log(e.currentTarget.hash);
+        let selectedLink = e.currentTarget.hash;
+        gsap.to(window, {duration: 1, scrollTo:{y:`${selectedLink}`, offsetY: 100}});
+    }
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", scrollLink);
+    });
+
+    //card appearing
+
+    const statisticsCards = document.querySelectorAll('.box-statistics');
+
+    statisticsCards.forEach((card) => {
+        gsap.set(card, {
+            opacity: 0,
+            y: 50
+        });
+
+        gsap.to(card, {
+            opacity: 1,
+            y: 0,
+            duration: 2,
+            scrollTrigger: {
+                trigger: card,
+                start: "top 80%",
+                end: "top 30%",
+                toggleActions: "play none none reverse",
+                markers: false
+            }
+        });
+    });
+
+    //FAQ 
+     const faqs = document.querySelectorAll('.faq');
+    faqs.forEach(faq => {
     faq.addEventListener("click", () => {
       faq.classList.toggle('openFaq');
     })
-  })
+    })
 })();
